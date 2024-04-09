8c2a7189a62567ef6a19d2a20f3e69025f5e33b8
ik012982i9 <jgallardome23dw@ikzubirimanteo.com>
2024-04-09 08:51:24 +0200

    BeforeAll y AfterAll, usos

--- src/test/java/com/pruebas/SmartPhoneServiceTest.java
+++ src/test/java/com/pruebas/SmartPhoneServiceTest.java
@@ -2,9 +2,9 @@ package com.pruebas;
 
 import static org.junit.jupiter.api.Assertions.*;
 import com.example.demo.service.SmartPhoneServiceImpl;
-import org.junit.Before;
-import org.junit.Test;
 import com.example.demo.service.SmartPhoneService;
+import org.junit.Test;
+import org.junit.Before;
 
 public class SmartPhoneServiceTest {
 
@@ -44,4 +44,5 @@ public class SmartPhoneServiceTest {
 
     //Las funciones lambda en Java se utilizan para proporcionar una forma concisa de expresar comportamientos sin la necesidad de definir una clase anónima o implementar una interfaz funcional.
     //@BeforeEach y @AfterEach son anotaciones utilizadas en JUnit para especificar métodos que se ejecutan antes y después de cada método de prueba en una clase de prueba, respectivamente. Pueden ser útiles para configurar el estado inicial y limpiar el estado después de cada prueba. En este caso no se utilizan ya que no hay que iniciar nada antes o despues de cada prueba.
+    //@BeforeAll y @AfterAll son anotaciones utilizadas en JUnit para especificar métodos que se ejecutan una vez antes y después de todos los métodos de prueba en una clase de prueba, respectivamente. Pueden ser útiles para configurar el estado inicial una vez y limpiar después de todas las pruebas.
 }
